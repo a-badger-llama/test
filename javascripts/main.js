0d31a00202800f1eff12443fa8d442c0cd5c3a5a
jonathanfox <jonathan.fox1991@gmail.com>
2022-10-18 23:19:46 -0400

    Add header subtitle and change secondary color and change header layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let menuToggle = document.getElementsByClassName("menu-toggle")[0];
 
   menuToggle.addEventListener("click", (e) => {
+    console.log("test")
     let display = document.getElementsByClassName("navbar")[0].style.display;
     if (display == "flex") {
       display = "none";
